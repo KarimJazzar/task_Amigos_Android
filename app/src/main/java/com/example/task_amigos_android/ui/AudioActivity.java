7803418cffedbd3008d5672b2e65290fd6d59e6c
Simranjit Singh <C0809865@mylambton.ca>
2022-02-05 00:46:50 +0530

    Added Audio Activity

---
+++ app/src/main/java/com/example/task_amigos_android/ui/AudioActivity.java
@@ -0,0 +1,16 @@
+package com.example.task_amigos_android.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.task_amigos_android.R;
+
+public class AudioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio);
+    }
+}
