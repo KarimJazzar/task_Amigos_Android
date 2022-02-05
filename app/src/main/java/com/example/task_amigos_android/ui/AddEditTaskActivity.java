dd218dd168fc878aec418b460c3f94ea98dd1510
febinrukfan772 <c0826772@mylambton.ca>
2022-02-05 14:29:15 -0500

    info view design part completed

--- app/src/main/java/com/example/task_amigos_android/ui/AddEditTaskActivity.java
+++ app/src/main/java/com/example/task_amigos_android/ui/AddEditTaskActivity.java
@@ -28,5 +28,35 @@ public class AddEditTaskActivity extends AppCompatActivity {
         FragmentManager fm = getSupportFragmentManager();
         adapter = new AddEditFrgAdapter(fm, getLifecycle());
         pager.setAdapter(adapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Add Info"));
+        tabLayout.addTab(tabLayout.newTab().setText("Add Sub Task"));
+        tabLayout.addTab(tabLayout.newTab().setText("Attach Media"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        pager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
     }
 }
