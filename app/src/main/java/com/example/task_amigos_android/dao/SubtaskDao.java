61e8c0b8a6115b347309dbbf04c2a70a84da1768
Daniel Miolan <miolan96@gmail.com>
2022-02-06 19:29:16 -0400

    Create Subtask data access objects.

---
+++ app/src/main/java/com/example/task_amigos_android/dao/SubtaskDao.java
@@ -0,0 +1,24 @@
+package com.example.task_amigos_android.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import com.example.task_amigos_android.entities.Subtask;
+
+import java.util.List;
+
+public interface SubtaskDao {
+    @Query("SELECT * FROM subtask WHERE task_id IN (:taskId)")
+    LiveData<List<Subtask>> loadAllByIds(int taskId);
+
+    @Insert
+    void insert(Subtask subtask);
+
+    @Update
+    void update(Subtask subtask);
+
+    @Delete
+    void delete(Subtask subtask);
+}
