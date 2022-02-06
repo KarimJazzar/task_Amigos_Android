f171b9a7bd8cabafb508b1574beb9daaca507106
Daniel Miolan <miolan96@gmail.com>
2022-02-06 19:31:38 -0400

    Create room database builder.

---
+++ app/src/main/java/com/example/task_amigos_android/database/AppDatabase.java
@@ -0,0 +1,30 @@
+package com.example.task_amigos_android.database;
+
+import android.content.Context;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+import com.example.task_amigos_android.dao.TaskDao;
+import com.example.task_amigos_android.entities.Category;
+import com.example.task_amigos_android.entities.Subtask;
+import com.example.task_amigos_android.entities.Task;
+
+@Database(entities = {Task.class, Category.class, Subtask.class}, version = 2)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static String DB_NAME = "amigos";
+    private static AppDatabase INSTANCE = null;
+
+    public abstract TaskDao taskDao();
+
+    public static synchronized AppDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE =  Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, DB_NAME)
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+
+        return INSTANCE;
+    }
+}
