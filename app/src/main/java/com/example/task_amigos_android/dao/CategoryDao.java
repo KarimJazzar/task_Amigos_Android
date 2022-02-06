1a2fb4423d89f7dff9a08197dc30da55ecfde235
Daniel Miolan <miolan96@gmail.com>
2022-02-06 19:28:41 -0400

    Create Category data access objects.

---
+++ app/src/main/java/com/example/task_amigos_android/dao/CategoryDao.java
@@ -0,0 +1,26 @@
+package com.example.task_amigos_android.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import com.example.task_amigos_android.entities.Category;
+
+import java.util.List;
+
+@Dao
+public interface CategoryDao {
+    @Query("SELECT * FROM category")
+    LiveData<List<Category>> getAll();
+
+    @Insert
+    void insert(Category tasks);
+
+    @Update
+    void update(Category task);
+
+    @Delete
+    void delete(Category task);
+}
