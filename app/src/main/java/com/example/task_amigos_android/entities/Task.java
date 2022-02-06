e4d0a468acde938c9110e356c59f15b4830f8a12
Daniel Miolan <miolan96@gmail.com>
2022-02-06 19:15:09 -0400

    Create Task entity.

---
+++ app/src/main/java/com/example/task_amigos_android/entities/Task.java
@@ -0,0 +1,109 @@
+package com.example.task_amigos_android.entities;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Task {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @ColumnInfo(name = "description")
+    private String description;
+
+    @ColumnInfo(name = "category")
+    private int category;
+
+    @ColumnInfo(name = "status")
+    private Boolean status;
+
+    @ColumnInfo(name = "image")
+    private List<String> images;
+
+    @ColumnInfo(name = "audio")
+    private List<String> audios;
+
+    @ColumnInfo(name = "due_date")
+    private Date dueDate;
+
+    @ColumnInfo(name = "creatation_data")
+    private Date creationDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    public List<String> getAudios() {
+        return audios;
+    }
+
+    public void setAudios(List<String> audios) {
+        this.audios =  audios;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
