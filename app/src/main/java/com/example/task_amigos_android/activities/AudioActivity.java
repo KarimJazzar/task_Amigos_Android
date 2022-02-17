d570d11addf7ff29f602d0597ab4d1a194e30127
Simranjit Singh <C0809865@mylambton.ca>
2022-02-16 19:01:02 -0500

    Fix permissions issue

--- app/src/main/java/com/example/task_amigos_android/activities/AudioActivity.java
+++ app/src/main/java/com/example/task_amigos_android/activities/AudioActivity.java
@@ -1,5 +1,6 @@
 package com.example.task_amigos_android.activities;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -7,6 +8,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -19,12 +21,13 @@ import com.example.task_amigos_android.R;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class AudioActivity extends AppCompatActivity {
 
-    Button record, choose,delete, save, stopRecord;
+    Button record, choose, delete, stopRecord;
     ImageButton play, stopAudio;
-    SeekBar seekbar;
     MediaRecorder mediaRecorder;
     MediaPlayer mediaPlayer;
     String AudioSavaPath = null;
@@ -37,47 +40,44 @@ public class AudioActivity extends AppCompatActivity {
         record = findViewById(R.id.recordButton);
         choose = findViewById(R.id.chooseButton);
         delete = findViewById(R.id.deleteButton);
-        save = findViewById(R.id.saveButton);
         play = findViewById(R.id.playButton);
         stopAudio = findViewById(R.id.stopAudioButton);
-        seekbar = findViewById(R.id.seekBar);
         stopRecord = findViewById(R.id.stopButton);
 
 
+        //Record button method to start and save recording
         record.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 if (checkPermissions() == true) {
 
-                    /*AudioSavaPath = Environment.getExternalStorageDirectory().getAbsolutePath()
-                            +"/"+"recordingAudio.mp3";*/
-                    AudioSavaPath = Environment.getDataDirectory().getAbsolutePath()
+                    AudioSavaPath = Environment.getExternalStorageDirectory().getAbsolutePath()
                             +"/"+"recordingAudio.mp3";
                     mediaRecorder = new MediaRecorder();
                     mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
                     mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-                    mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+                    mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
                     mediaRecorder.setOutputFile(AudioSavaPath);
 
                     try {
                         mediaRecorder.prepare();
                         mediaRecorder.start();
                         Toast.makeText(AudioActivity.this, "Recording started", Toast.LENGTH_SHORT).show();
-
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
 
                 }else {
                     ActivityCompat.requestPermissions(AudioActivity.this,new String[]{
-                            Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                            Manifest.permission.MANAGE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE
+                            Manifest.permission.RECORD_AUDIO,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
                     },1);
                 }
             }
         });
 
+        //Stop button to stop recording
         stopRecord.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,26 +88,39 @@ public class AudioActivity extends AppCompatActivity {
         });
 
 
+        //Play button to play the recording
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 mediaPlayer = new MediaPlayer();
-                try {
-                    File path = android.os.Environment.getExternalStorageDirectory();
-                    mediaPlayer.setDataSource(path + "/t1.mp3");
 
-                    //mediaPlayer = MediaPlayer.create(AudioActivity.this, R.raw.sampleaudio);
-                    mediaPlayer.setDataSource(AudioSavaPath);
-                    mediaPlayer.prepare();
+                //Testing for media player
+                /*mediaPlayer = MediaPlayer.create(AudioActivity.this, R.raw.sampleaudio);
+                mediaPlayer.start();
+                Toast.makeText(AudioActivity.this, "Start playing", Toast.LENGTH_SHORT).show();*/
+
+                if (AudioSavaPath != null){
+                    try {
+                        mediaPlayer.setDataSource(AudioSavaPath);
+                        //mediaPlayer.prepare();
+                        mediaPlayer.start();
+                        Toast.makeText(AudioActivity.this, "Start playing", Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+
+                    //Change this to play any existing audio from choose button
+                    mediaPlayer = MediaPlayer.create(AudioActivity.this, R.raw.sampleaudio);
                     mediaPlayer.start();
                     Toast.makeText(AudioActivity.this, "Start playing", Toast.LENGTH_SHORT).show();
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
+
             }
         });
 
+        //Stop button to stop the playing
         stopAudio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -118,11 +131,28 @@ public class AudioActivity extends AppCompatActivity {
                     Toast.makeText(AudioActivity.this, "Stopped playing", Toast.LENGTH_SHORT).show();
                 }
             }
+
         });
 
+        //Delete button to delete the recording
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
 
+            }
 
+        });
+
+        //Choose button to choose an existing audio
+        choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+
+        });
     }
 
     private boolean checkPermissions() {
@@ -130,17 +160,14 @@ public class AudioActivity extends AppCompatActivity {
                 Manifest.permission.RECORD_AUDIO);
         int second = ActivityCompat.checkSelfPermission(getApplicationContext(),
                 Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        int  third = ActivityCompat.checkSelfPermission(getApplicationContext(),
-                Manifest.permission.MANAGE_EXTERNAL_STORAGE);
-        int fourth = ActivityCompat.checkSelfPermission(getApplicationContext(),
+        int third = ActivityCompat.checkSelfPermission(getApplicationContext(),
                 Manifest.permission.READ_EXTERNAL_STORAGE);
 
         return first == PackageManager.PERMISSION_GRANTED &&
                 second == PackageManager.PERMISSION_GRANTED &&
-                third == PackageManager.PERMISSION_GRANTED &&
-                fourth == PackageManager.PERMISSION_GRANTED;
+                third == PackageManager.PERMISSION_GRANTED;
     }
 
-    //Pending - Delete the save button, Delete recording button and choose audio button.
+    //Pending - Delete button and choose audio button.
 
 }
